58ddb94c549191ec11fbb9cfc60fc502f342648b
antoniogisondi <anto.gisondi@tiscali.it>
2023-07-31 23:25:22 +0200

    Style of show page done

--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -2,9 +2,24 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row mt-4">
             <div class="col">
-                Info fumetto
+                @if(!empty($comic->thumb && $comic->cover_image && $comic->thumb2))
+                <img src="{{ $comic->thumb }}" class="img-fluid">
+                @else
+                    <h5>Immagine non disponibile</h5>
+                @endif
+            </div>
+
+            <div class="col">
+                <h3>{{ $comic->title }}</h3>
+                <p>{{ $comic->description }}</p>
+                <h6><strong>Series:</strong> {{ $comic->series }}</h6>
+                <h6><strong>Type:</strong> {{ $comic->type }}</h6>
+                <h6><strong>Price:</strong> {{ $comic->price }}</h6>
+                <h6><strong>Date:</strong> {{ $comic->sale_date }}</h6>
+                <p><strong>Artists:</strong> {{ $comic->artists }}</p>
+                <p><strong>Writers:</strong> {{ $comic->writers }}</p>
             </div>
         </div>
     </div>
